55c2064abde884b0299a6a103a8385cc27e115b2
Eric J Wilhite <EricJWilhite@gmail.com>
2019-10-27 14:10:31 -0700

    Done.

---
+++ app/models/super_hero.rb
@@ -0,0 +1,16 @@
+class SuperHero
+    attr_reader :name, :power, :bio
+
+    HEROES = []
+
+    def initialize(details)
+        @name = details[:name]
+        @power = details[:power]
+        @bio = details[:bio]
+        HEROES << self
+    end
+
+    def self.all
+        HEROES
+    end
+end
